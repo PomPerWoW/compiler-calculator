85486aa9476017d77e253ccda583a49d20a39858
lollipop <hastyfasd@gmai.com>
2025-02-05 22:15:48 +0700

    finish handle ( parathensis

--- src/code_generator/code_generator.py
+++ src/code_generator/code_generator.py
@@ -23,32 +23,41 @@ class CodeGenerator:
     def generate(self, parsed_output, tokenized_output):
         """
         Generate assembly-like instructions from parsed and tokenized outputs.
-        
-        :param parsed_output: String containing parsed expressions and errors
-        :param tokenized_output: List of tokenized expressions
-        :return: List of assembly-like instructions
         """
         self.assembly_code = []
         self.error_encountered = False
         parsed_lines = parsed_output.split('\n')
-        # remove '' from parsed_lines list
         parsed_lines = list(filter(None, parsed_lines))
                 
         for i, tokens in enumerate(tokenized_output):
-            # if self.error_encountered:
-            #     continue
-            
             if 'SyntaxError' in parsed_lines[i] or 'Undefined variable' in parsed_lines[i] or 'Index out of range' in parsed_lines[i]:
                 self.assembly_code.append('ERROR\n')
                 continue
             
             token_list = tokens.split()
+            parsed_line = parsed_lines[i].strip()
             
             try:
-                # Handle different operations
-                # handle (
-                if 'LPAREN' in tokens:
-                    self._handle_parenthesis(token_list)
+                # First check for explicit parentheses tokens
+                if 'LPAREN' in tokens and 'RPAREN' in tokens:
+                    # Filter out parentheses tokens and handle inner expression
+                    inner_tokens = [token for token in token_list if 'LPAREN' not in token and 'RPAREN' not in token]
+                    if 'PLUS' in tokens:
+                        self._handle_addition(inner_tokens)
+                    elif 'TIMES' in tokens:
+                        self._handle_multiplication(inner_tokens)
+                    elif 'DIVIDE' in tokens:
+                        self._handle_division(inner_tokens)
+                # Then check for compound expressions
+                elif parsed_line.startswith('(') and '=' in parsed_line and ('+' in parsed_line or '*' in parsed_line or '/' in parsed_line):
+                    operator = None
+                    if '+' in parsed_line:
+                        operator = 'PLUS'
+                    elif '*' in parsed_line:
+                        operator = 'TIMES'
+                    elif '/' in parsed_line:
+                        operator = 'DIVIDE'
+                    self._handle_compound_expression(token_list, operator)
                 elif 'LBRACKET' in tokens:
                     self._handle_list_operation(token_list)
                 elif 'ASSIGNMENT' in tokens:
@@ -71,7 +80,37 @@ class CodeGenerator:
             self.assembly_code.append('')
         
         return self.assembly_code
-
+    def _handle_compound_expression(self, tokens, operator):
+        """
+        Handle compound expressions like (z=(3+2)), (d=(3*2)), (e=(3/2))
+        """
+        # Parse out the components
+        var_name = tokens[0].split('/')[0]
+        left_val = tokens[2].split('/')[0]
+        right_val = tokens[4].split('/')[0]
+        
+        # Get registers
+        r_left = self.get_register()
+        r_right = self.get_register()
+        r_result = self.get_register()
+        
+        # Generate assembly code
+        self.assembly_code.append(f'LD {r_left} {self._load_value(left_val)}')
+        self.assembly_code.append(f'LD {r_right} {self._load_value(right_val)}')
+        
+        if operator == 'PLUS':
+            self.assembly_code.append(f'ADD.i {r_result} {r_left} {r_right}')
+        elif operator == 'TIMES':
+            self.assembly_code.append(f'MUL.i {r_result} {r_left} {r_right}')
+        elif operator == 'DIVIDE':
+            self.assembly_code.append(f'DIV.i {r_result} {r_left} {r_right}')
+        elif operator == 'NOT_EQUAL':
+            self.assembly_code.append(f'NE.f {r_result} {r_left} {r_right}')
+        elif operator == 'ASSIGNMENT':
+            self.assembly_code.append(f'ST @{var_name} {r_result}')            
+            
+        self.assembly_code.append(f'ST @{var_name} {r_result}')
+        
     def _load_value(self, value):
         """Returns correctly formatted value loading instruction"""
         return f"#{value}" if value.isdigit() or value.replace('.', '', 1).isdigit() else f"@{value}"
@@ -79,27 +118,80 @@ class CodeGenerator:
     def _handle_parenthesis(self, tokens):
         """
         Handle expressions within parentheses by processing the inner expression.
-        Ignores the parentheses tokens and processes the content within.
+        Now handles nested expressions properly.
         """
-        # Filter out parenthesis tokens
+        # Filter out the outer LPAREN/RPAREN tokens
         inner_tokens = [token for token in tokens if 'LPAREN' not in token and 'RPAREN' not in token]
         
-        # Check what operation is inside the parentheses
-        if any('LBRACKET' in token for token in inner_tokens):
-            self._handle_list_operation(inner_tokens)
-        elif any('PLUS' in token for token in inner_tokens):
-            self._handle_addition(inner_tokens)
-        elif any('TIMES' in token for token in inner_tokens):
-            self._handle_multiplication(inner_tokens)
-        elif any('DIVIDE' in token for token in inner_tokens):
-            self._handle_division(inner_tokens)
-        elif any('NOT_EQUAL' in token for token in inner_tokens):
-            self._handle_not_equal(inner_tokens)
-        elif any('ASSIGNMENT' in token for token in inner_tokens):
-            self._handle_assignment(inner_tokens)
-        else:
-            self.assembly_code.append('# UNHANDLED OPERATION INSIDE PARENTHESES')
+        # Check for assignment with expression
+        if any('ASSIGNMENT' in token for token in inner_tokens):
+            # Find position of ASSIGNMENT
+            assign_pos = next(i for i, token in enumerate(inner_tokens) if 'ASSIGNMENT' in token)
+            var_name = inner_tokens[0].split('/')[0]
             
+            # Process right-hand side expression
+            right_tokens = inner_tokens[assign_pos + 1:]
+            
+            if any('PLUS' in token for token in right_tokens):
+                # Handle addition first
+                left_val = right_tokens[0].split('/')[0]
+                right_val = right_tokens[2].split('/')[0]
+                
+                r_left = self.get_register()
+                r_right = self.get_register()
+                r_result = self.get_register()
+                
+                self.assembly_code.append(f'LD {r_left} {self._load_value(left_val)}')
+                self.assembly_code.append(f'LD {r_right} {self._load_value(right_val)}')
+                self.assembly_code.append(f'ADD.i {r_result} {r_left} {r_right}')
+                self.assembly_code.append(f'ST @{var_name} {r_result}')
+            
+            elif any('TIMES' in token for token in right_tokens):
+                # Handle multiplication first
+                left_val = right_tokens[0].split('/')[0]
+                right_val = right_tokens[2].split('/')[0]
+                
+                r_left = self.get_register()
+                r_right = self.get_register()
+                r_result = self.get_register()
+                
+                self.assembly_code.append(f'LD {r_left} {self._load_value(left_val)}')
+                self.assembly_code.append(f'LD {r_right} {self._load_value(right_val)}')
+                self.assembly_code.append(f'MUL.i {r_result} {r_left} {r_right}')
+                self.assembly_code.append(f'ST @{var_name} {r_result}')
+                
+            elif any('DIVIDE' in token for token in right_tokens):
+                # Handle division first
+                left_val = right_tokens[0].split('/')[0]
+                right_val = right_tokens[2].split('/')[0]
+                
+                r_left = self.get_register()
+                r_right = self.get_register()
+                r_result = self.get_register()
+                
+                self.assembly_code.append(f'LD {r_left} {self._load_value(left_val)}')
+                self.assembly_code.append(f'LD {r_right} {self._load_value(right_val)}')
+                self.assembly_code.append(f'DIV.i {r_result} {r_left} {r_right}')
+                self.assembly_code.append(f'ST @{var_name} {r_result}')
+            
+            else:
+                # Simple assignment
+                value = right_tokens[0].split('/')[0]
+                r_val = self.get_register()
+                self.assembly_code.append(f'LD {r_val} {self._load_value(value)}')
+                self.assembly_code.append(f'ST @{var_name} {r_val}')
+        
+        else:
+            # Handle non-assignment expressions
+            if any('PLUS' in token for token in inner_tokens):
+                self._handle_addition(inner_tokens)
+            elif any('TIMES' in token for token in inner_tokens):
+                self._handle_multiplication(inner_tokens)
+            elif any('DIVIDE' in token for token in inner_tokens):
+                self._handle_division(inner_tokens)
+            elif any('NOT_EQUAL' in token for token in inner_tokens):
+                self._handle_not_equal(inner_tokens)
+                
 
     def _handle_assignment(self, tokens):
         """
@@ -250,61 +342,53 @@ class CodeGenerator:
 # Example usage
 symbol_table = {"x": "LIST", "z": "VAR", "d": "VAR", "e": "VAR", "g": "VAR"}
 parsed_output = """
-(23+8)
-(2.5*0)
-SyntaxError at line 3, pos 2
-(x=5)
-(10*x)
-Undefined variable 'y' at line 6, pos 3
-(x!=5)
-(2+5)
-(x=(list[(2)]))
-((x[(0)])+(x[(1)]))
-Index 4 out of range for list 'x' of size 2 at line 11, pos 1
-((x[(1)])=2)
-((x[(0)])+2)
-(z=(3+2))
-(d=(3*2))
-(e=(3/2))
-SyntaxError at line 17, pos 7
-(g=2)
+    (23+8)
+    (2.5*0)
+    SyntaxError at line 3, pos 2
+    (x=5)
+    (10*x)
+    Undefined variable 'y' at line 6, pos 3
+    (x!=5)
+    (2+5)
+    (x=(list[(2)]))
+    ((x[(0)])+(x[(1)]))
+    Index 4 out of range for list 'x' of size 2 at line 11, pos 1
+    ((x[(1)])=2)
+    ((x[(0)])+2)
+    (z=(3+2))
+    (d=(3*2))
+    (e=(3/2))
+    SyntaxError at line 17, pos 7
+    (g=2)
 """
 
 tokenized_output = [
-"23/INT +/PLUS 8/INT",
-"2.5/REAL */TIMES 0/INT",
-"5/INT NUM/VAR ^/POW 3.0/REAL",
-"x/VAR =/ASSIGNMENT 5/INT",
-"10/INT */TIMES x/VAR",
-"x/VAR =/ASSIGNMENT y/VAR",
-"x/VAR !=/NOT_EQUAL 5/INT",
-"(/LPAREN 2/INT +/PLUS 5/INT )/RPAREN",
-"x/VAR =/ASSIGNMENT list/LIST [/LBRACKET 2/INT ]/RBRACKET",
-"x/VAR [/LBRACKET 0/INT ]/RBRACKET +/PLUS x/VAR [/LBRACKET 1/INT ]/RBRACKET",
-"x/VAR [/LBRACKET 4/INT ]/RBRACKET",
-"x/VAR [/LBRACKET 1/INT ]/RBRACKET =/ASSIGNMENT 2/INT",
-"x/VAR [/LBRACKET 0/INT ]/RBRACKET +/PLUS 2/INT",
-"z/VAR =/ASSIGNMENT 3/INT +/PLUS 2/INT",
-"d/VAR =/ASSIGNMENT 3/INT */TIMES 2/INT",
-"e/VAR =/ASSIGNMENT 3/INT //DIVIDE 2/INT",
-"f/VAR =/ASSIGNMENT 3/INT %/ERR 2/INT",
-"g/VAR =/ASSIGNMENT 2/INT",
-]
-
-parsed_output_test = """
-# (z=(3+2))
-# (d=(3*2))
-# (e=(3/2))
-# SyntaxError at line 17, pos 7
-# (g=2)
-"""
-
-tokenized_output_test = [
+    "23/INT +/PLUS 8/INT",
+    "2.5/REAL */TIMES 0/INT",
+    "5/INT NUM/VAR ^/POW 3.0/REAL",
+    "x/VAR =/ASSIGNMENT 5/INT",
+    "10/INT */TIMES x/VAR",
+    "x/VAR =/ASSIGNMENT y/VAR",
+    "x/VAR !=/NOT_EQUAL 5/INT",
+    "(/LPAREN 2/INT +/PLUS 5/INT )/RPAREN",
+    "x/VAR =/ASSIGNMENT list/LIST [/LBRACKET 2/INT ]/RBRACKET",
+    "x/VAR [/LBRACKET 0/INT ]/RBRACKET +/PLUS x/VAR [/LBRACKET 1/INT ]/RBRACKET",
+    "x/VAR [/LBRACKET 4/INT ]/RBRACKET",
+    "x/VAR [/LBRACKET 1/INT ]/RBRACKET =/ASSIGNMENT 2/INT",
+    "x/VAR [/LBRACKET 0/INT ]/RBRACKET +/PLUS 2/INT",
     "z/VAR =/ASSIGNMENT 3/INT +/PLUS 2/INT",
     "d/VAR =/ASSIGNMENT 3/INT */TIMES 2/INT",
     "e/VAR =/ASSIGNMENT 3/INT //DIVIDE 2/INT",
     "f/VAR =/ASSIGNMENT 3/INT %/ERR 2/INT",
-    "g/VAR =/ASSIGNMENT 2/INT"
+    "g/VAR =/ASSIGNMENT 2/INT",
+]
+
+parsed_output_test = """
+    (2+5)
+"""
+
+tokenized_output_test = [
+    "(/LPAREN 2/INT +/PLUS 5/INT )/RPAREN",
 ]
 
 generator = CodeGenerator(symbol_table)
